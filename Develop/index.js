2a7766f6a92038318d78fc61cf0180540a6cba7f
joseobm92 <joseobm92@gmail.com>
2022-10-23 14:39:38 -0400

    fixed index.js packages

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-const util = require('util');
 
 
 // TODO: Create an array of questions for user input
